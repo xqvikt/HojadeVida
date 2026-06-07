18e49e00937f800572eeb18a7282d1340e288d8f
Xavier Garboa <xgarboa@gmail.com>
2026-06-07 15:25:28 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const boton = document.getElementById("boton");
+
+boton.addEventListener("click", function(){
+
+    const titulo = document.getElementById("titulo");
+
+    titulo.style.color = "brown";
+    
+    alert("Color cambiado correctamente");
+
+});
